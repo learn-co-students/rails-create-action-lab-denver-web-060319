b9010def3d6ad5e9d89400182f7adf6e5949af3f
“Patrick <pctennislessons@gmail.com>
2019-07-02 10:09:00 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,34 @@
 class StudentsController < ApplicationController
   
   def index
-    @students = Student.all
+    @students = all_students_array
   end
 
   def show
-    @student = Student.find(params[:id])
+    @student = find_student
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.create(allowed_params)
+    # byebug
+    redirect_to student_path(@student)
   end
 
+  private
+
+  def allowed_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
+
+  def find_student
+    Student.find(params[:id])
+  end
+
+  def all_students_array
+    Student.all
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,7 @@
 class Student < ActiveRecord::Base
-  def to_s
+  validates :first_name, presence: true 
+  # validates :last_name, presence: true 
+  def to_full_name
     self.first_name + " " + self.last_name
   end
 end
